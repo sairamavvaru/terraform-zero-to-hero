ef0ce5721f39fb34534f0a77725e0d089f8ba5cc
sairamavvaru <sairamavvaru203@gmail.com>
2025-07-15 19:43:55 +0530

    change on some code

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -6,5 +6,5 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-020cba7c55df1f615" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-0f3ac6dd858ac59d7". # replace this
+  subnet_id_value = "subnet-0f3ac6dd858ac59d7" # replace this
 }
