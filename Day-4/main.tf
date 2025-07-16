18fc577553743b0a2df841a6caed38c13892c5f6
sairamavvaru <sairamavvaru203@gmail.com>
2025-07-16 18:09:40 +0530

    add on new file

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -4,21 +4,11 @@ provider "aws" {
 
 resource "aws_instance" "abhishek" {
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+  ami = "ami-020cba7c55df1f615" # change this
+  subnet_id = "subnet-0f3ac6dd858ac59d7" # change this
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "abhishek-s3-demo-xyz" # change this
 }
 
-resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "terraform-lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
