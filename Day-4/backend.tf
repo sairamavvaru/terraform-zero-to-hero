6f1abfcef9cd795131f78b883ee19efbb1565cda
sairamavvaru <sairamavvaru203@gmail.com>
2025-07-16 18:40:25 +0530

    added new code

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
+    bucket = "saiarm-s3-demo-xxx"
+    region ="us-east-1"
+    key = "sairam/terraform.tfstate"
   }
 }
