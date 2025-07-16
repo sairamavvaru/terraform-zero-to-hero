c7e6a9bdad1fa0ef960751da6b40dbfec13b70c4
sairamavvaru <sairamavvaru203@gmail.com>
2025-07-16 18:13:26 +0530

    change for new name

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "abhishek" {
+resource "aws_instance" "sairam" {
   instance_type = "t2.micro"
   ami = "ami-020cba7c55df1f615" # change this
   subnet_id = "subnet-0f3ac6dd858ac59d7" # change this
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "sairam-s3-demo-xyz" # change this
 }
 
